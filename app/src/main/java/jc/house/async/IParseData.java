b252c483b012bd5643c516d1f647a95cf0e070ad
nannanIT <759010589@qq.com>
2015-12-11 22:19:41 +0800

    整理代码，添加了MThreadPoll

---
+++ app/src/main/java/jc/house/async/IParseData.java
@@ -0,0 +1,13 @@
+package jc.house.async;
+
+import java.util.List;
+
+import jc.house.global.FetchType;
+import jc.house.models.BaseModel;
+
+/**
+ * Created by hzj on 2015/12/11.
+ */
+public interface IParseData {
+    void onTaskCompleted(List<BaseModel> lists, FetchType fetchType);
+}
---
+++ app/src/main/java/jc/house/async/MThreadPool.java
@@ -0,0 +1,7 @@
+package jc.house.async;
+
+/**
+ * Created by hzj on 2015/12/11.
+ */
+public class MThreadPool {
+}
